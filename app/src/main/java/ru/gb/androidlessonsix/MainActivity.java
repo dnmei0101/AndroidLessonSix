c6001c983ed955201dc4ca69f4c1a2d7dba8397f
Denis S <ruimei@vivaldi.net>
2022-06-04 03:55:35 +0300

    Done!

--- app/src/main/java/ru/gb/androidlessonsix/MainActivity.java
+++ app/src/main/java/ru/gb/androidlessonsix/MainActivity.java
@@ -2,13 +2,34 @@ package ru.gb.androidlessonsix;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.res.Configuration;
 import android.os.Bundle;
 
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
+
+    final String NOTES = "notes";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        String[] countries = getResources().getStringArray(R.array.country_names);
+        String[] capitals = getResources().getStringArray(R.array.country_capital);
+        ArrayList<Note> notes = Note.initArrayNote(capitals, countries);
+
+        NoteFragment noteFragment = new NoteFragment();
+        Bundle bundle = new Bundle();
+        bundle.putParcelableArrayList(NOTES, notes);
+        noteFragment.setArguments(bundle);
+
+
+        if (savedInstanceState == null) {
+            getSupportFragmentManager()
+                    .beginTransaction()
+                    .replace(R.id.fragment_container, noteFragment)
+                    .commit();
+        }
     }
 }
